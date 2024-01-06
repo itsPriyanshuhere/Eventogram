75c9c302f169d6ded17d16ffe6cb6c848f8c6f41
itsPriyanshuhere <priyanshupandey256@gmail.com>
2024-01-06 17:03:17 +0530

    Added shows and event detail page where the payment price gets dynamically updated.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import { Route, Routes } from "react-router-dom"
+import Home from "./components/Home"
+import EventDetail from "./components/EventDetail"
+import NoMatch from "./components/NoMatch"
+import Login from "./components/Login"
+import SignUp from "./components/SignUp"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Navbar />
+    <Routes>
+      <Route path = "/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<SignUp />} />
+      <Route path="/shows" element={<Hero />} />
+      <Route path="/events/:id" element={<EventDetail />} />
+      <Route path="*" element={<NoMatch />} />
+    </Routes>
+    </>
+  )
 }
-
-export default App;
