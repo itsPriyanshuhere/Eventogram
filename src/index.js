72b23024dd24a079f8632fbbf61997293573aa5c
Samar Ahar <Tusharahar1906@gmail.com>
2024-01-07 09:32:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
   <BrowserRouter>
-    <div className=''>
-      <App />
-    </div>
+    <App />
+    <Toaster/>
   </BrowserRouter>
-  </React.StrictMode>
-);
+    
 
-reportWebVitals();
+);
