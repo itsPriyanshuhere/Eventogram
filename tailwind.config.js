55ee131def34165a397c2fc4c27b970b792e6ebf
Samar Ahar <Tusharahar1906@gmail.com>
2024-01-07 09:47:41 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    fontFamily: {
-      'sans': ['ui-sans-serif', 'system-ui'],
-      'serif': ['ui-serif', 'Georgia'],
-      'mono': ['ui-monospace', 'SFMono-Regular'],
-      'display': ['Oswald'],
-      'body': ['"Open Sans"'],
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        'sans': ['ui-sans-serif', 'system-ui'],
+        'serif': ['ui-serif', 'Georgia'],
+        'mono': ['ui-monospace', 'SFMono-Regular'],
+        'display': ['Oswald'],
+        'body': ['"Open Sans"'],
+      },
+      colors: {
+        richblack: {
+          5: "#F1F2FF",
+          25: "#DBDDEA",
+          100: "#AFB2BF",
+          200: "#999DAA",
+          700: "#2C333F",
+          800: "#161D29",
+          900: "#000814",
+        },
+        blue: {
+          100: "#47A5C5",
+        },
+        pink: {
+          200: "#EF476F",
+        },
+        yellow: {
+          50: "#FFD60A",
+        },
+      },
     },
-    extend: {},
   },
   plugins: [],
-}
+};
